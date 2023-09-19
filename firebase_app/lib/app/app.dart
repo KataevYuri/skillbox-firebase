da6ca970f73c453a18ff1d81d3e1ad4d9c0dcd2d
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2023-09-19 21:48:23 +0800

    first run error

--- firebase_app/lib/app/app.dart
+++ firebase_app/lib/app/app.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_app/app/theme.dart';
 import 'package:firebase_app/presentation/pages/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -9,7 +12,46 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: lightTheme,
-      home: const HomePage(),
+      home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.userChanges(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: ElevatedButton(
+                  onPressed: singIn,
+                  child: const Text('Login'),
+                ),
+              );
+            } else {
+              return const HomePage();
+            }
+          }),
     );
+  }
+
+  void singIn() {
+    if (kIsWeb) {
+      //signInWithGoogleWeb();
+    } else {
+      signInWithGoogle();
+    }
+  }
+
+  Future<UserCredential?> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
