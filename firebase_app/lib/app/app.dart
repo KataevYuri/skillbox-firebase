bfd2f626188831bc2b84177fd01961f1c27693db
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2023-09-25 22:30:29 +0800

    1

--- firebase_app/lib/app/app.dart
+++ firebase_app/lib/app/app.dart
@@ -1,39 +1,48 @@
 import 'package:firebase_app/app/theme.dart';
+import 'package:firebase_app/firebase/firbase_service.dart';
 import 'package:firebase_app/presentation/pages/home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: lightTheme,
-      home: StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.userChanges(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
+        theme: lightTheme,
+        home: isLoading == false
+            ? Center(
                 child: ElevatedButton(
                   onPressed: singIn,
                   child: const Text('Login'),
                 ),
-              );
-            } else {
-              return const HomePage();
-            }
-          }),
-    );
+              )
+            : const HomePage());
   }
 
-  void singIn() {
-    if (kIsWeb) {
-      //signInWithGoogleWeb();
-    } else {
-      signInWithGoogle();
+  void singIn() async {
+    if (!kIsWeb) {
+      FirebaseService service = FirebaseService();
+      try {
+        await service.signInwithGoogle();
+      } catch (e) {
+        if (e is FirebaseAuthException) {
+          debugPrint(e.message!);
+        }
+      }
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
@@ -51,5 +60,6 @@ class MainApp extends StatelessWidget {
 
       return await FirebaseAuth.instance.signInWithCredential(credential);
     }
+    return null;
   }
 }
