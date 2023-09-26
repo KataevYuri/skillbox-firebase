e31caea9ec901f99e2e6c3b1a5ffd8e995ff49d4
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2023-09-26 22:14:04 +0800

    Update main.dart

--- firebase_app/lib/main.dart
+++ firebase_app/lib/main.dart
@@ -7,7 +7,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-      // options: DefaultFirebaseOptions.currentPlatform,
-      );
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
